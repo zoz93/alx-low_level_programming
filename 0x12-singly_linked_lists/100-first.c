5434b6e74888a1d7a7fdd3ac696ee7605dc1b586
zoz93 <abdelazizelhemaily@gmail.com>
2023-04-27 03:45:31 -0700

    100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+	return (0);
+}
