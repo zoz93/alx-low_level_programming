2d452e072dd58c79cc3213159823041097feaf92
zoz93 <abdelazizelhemaily@gmail.com>
2023-05-02 19:36:49 -0700

    h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -28,18 +28,12 @@ size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
-/*prototype function that frees a listint_t list.*/
 void free_listint2(listint_t **head);
-/*prototype function that frees a listint_t list.*/
 int pop_listint(listint_t **head);
-/*prototype function that deletes the head node of a listint_t linked list*/
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
-/*prototype function that returns the nth node of a listint_t linked list.*/
 int sum_listint(listint_t *head);
-/*prototype function that returns the sum of all the data (n) of a listint_t.*/
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
-/*prototype function that inserts a new node at a given position.*/
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
-/*prototy pefunction that deletes the node at index index of a listint_t.*/
+
 
 #endif /* LISTS_H */
