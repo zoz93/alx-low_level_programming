a2afd204ae4c2a9548e6e3d68005a3042ca116f4
zoz93 <abdelazizelhemaily@gmail.com>
2023-05-02 19:26:29 -0700

    7.1

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -5,4 +5,12 @@
  * @head:pointer value.
  * @index: value.
  * Return: Always 0 (Success)
- *
+ **/
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *node;
+	unsigned int a = 0;
+
+	node = head;
+}
