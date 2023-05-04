77422f37347ff44ffef2c92520da62669ff720a9
zoz93 <abdelazizelhemaily@gmail.com>
2023-05-04 14:24:30 -0700

    2.0

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_bit - entry point.
+ * @index:pointer value.
+ * @n: int value.
+ * Return: always 0 (success)
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int i;
+
+	if (index > 64)
+	{
+		return (-1);
+	}
+	i = n >> index;
+	return (i & 1);
+}
+
